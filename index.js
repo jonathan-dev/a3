cf674a7858ae8f81bc89785326a2a92f9fcdde9e
KevinVujic <kevinvujic@yahoo.de>
2017-08-20 17:20:00 +1000

    Created upload directory, uploads now stored in specified upload directory

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ import formidable from 'express-formidable'
 const PORT = 8000
 const app = express()
 
-app.use(formidable())
+app.use(formidable({
+    uploadDir: __dirname + '/public/uploads'
+}))
+
 app.use('/public', express.static('public'));
 
 app.get('/upload', (req, res) => {
@@ -14,6 +17,7 @@ app.get('/upload', (req, res) => {
 app.post('/upload', (req, res) => {
     req.fields; // contains non-file fields
     req.files; // contains files
+    console.log(req.files);
 })
 
 app.listen(
