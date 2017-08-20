2cbb2af4c53e386d490f2b0987638062cd8bf2cf
KevinVujic <kevinvujic@yahoo.de>
2017-08-20 16:58:59 +1000

    Implemented upload_page with included form

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import formidable from 'express-formidable'
 
 const PORT = 8000
 const app = express()
+
+app.use(formidable())
+
+app.post('/upload', (req, res) => {
+    req.fields; // contains non-file fields
+    req.files; // contains files
+})
 
 app.listen(
     PORT,
