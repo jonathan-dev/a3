a13cea0e17988115d35a3099ba2db2ebe36b3a41
Jonathan Drude <jonathan.drude@mni.thm.de>
2017-08-21 20:05:44 +1000

    commenting the index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-import express from 'express'
-import formidable from 'express-formidable'
+import express from 'express' // express webserver framework
+import formidable from 'express-formidable' // middleware for handling file uploads
 
-const PORT = 8000
-const app = express()
+const PORT = 8000 // port express is running on
+const app = express() // init express
 
+// apply middleware for handling file uploads
 app.use(formidable({
     uploadDir: __dirname + '/public/uploads'
 }))
 
+// ---- ROUTES ----
+
+// serve all files in the public folder statically
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
@@ -27,19 +31,18 @@ app.get('/post/id', (req, res) => {
 })
 
 app.post('/upload', (req, res) => {
-    req.fields; // contains non-file fields
-    req.files; // contains files
+    // NOTE: req.fields contains all data except file data
+    // NOTE: req.file contains all file related data
+    // store image data in variable
     var image = {
         title: req.fields.title,
         path: req.files.upload.path
     };
-
-    console.log("Image saved!:");
-    console.log(image);
-    // TODO save in database
-    res.redirect('/');
+    // TODO: save image infos in database
+    res.redirect('/'); // go back to home
 })
 
+// make the server listen on the specified port
 app.listen(
     PORT,
     () => console.log(`server listening on http://localhost:${PORT}`)
