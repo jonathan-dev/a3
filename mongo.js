1c28707ca70b39da6712e070152dae0d1c0b5491
Jonathan Drude <jonathan.drude@mni.thm.de>
2017-08-21 20:20:42 +1000

    adding comments to mongo.js

--- mongo.js
+++ mongo.js
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose'
-import casual from 'casual';
-import _ from 'lodash';
+import mongoose from 'mongoose' // connection to the mongodb
+import casual from 'casual'; // creating mock data
 
+// location of the mongodb
 const MONGO = 'mongodb://localhost/a3'
 
 mongoose.connect(MONGO, {
-  useMongoClient: true
+  useMongoClient: true // needed to get rid of a error message
 })
 
 let db = mongoose.connection
@@ -32,8 +32,10 @@ let tagSchema = mongoose.Schema({
 let Post = mongoose.model('Post', postSchema)
 let Tag = mongoose.model('Tag', tagSchema)
 
-async function initDatabase() {
 
+
+// This method initializes the database if it is empty
+async function initDatabase() {
   //insert tags
   const tags = await Tag.find()
   if (tags.length === 0) {
@@ -53,7 +55,10 @@ async function initDatabase() {
     const tagdata = await Tag.find()
     let ps = []
     for (let i = 0; i < 10; i++) {
-      let randarr = Array.apply(null, {length: casual.integer(1, 5)}).map(() => {
+      let randarr = Array.apply(null, {
+          length: casual.integer(1, 5)
+        })
+        .map(() => {
           return tagdata[casual.integer(1, 9)]._id
         })
       let p = await new Post({
@@ -71,7 +76,7 @@ async function initDatabase() {
 initDatabase()
 
 
-
+// exporting functionality
 export default {
   getPost(id) {
     return Post.findById(id)
