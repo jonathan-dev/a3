2b8f4ac654cd4d5d8a107e5e4c1a07c676eec0de
KevinVujic <kevinvujic@yahoo.de>
2017-08-21 20:23:13 +1000

    added route functionality comments on each route in index.js

--- index.js
+++ index.js
@@ -14,28 +14,32 @@ app.use(formidable({
 // serve all files in the public folder statically
 app.use('/public', express.static('public'));
 
+// serve the main page (hot page) to the client
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/views/hot_page.html");
 })
 
+// serve the post upload page to the client
 app.get('/upload', (req, res) => {
     res.sendFile(__dirname + "/public/views/upload_page.html");
 })
 
+// serve the edit post page to the client
 app.get('/edit', (req, res) => {
     res.sendFile(__dirname + "/public/views/edit_post_page.html");
 })
 
+// serve the post detail page to the client TODO serve specified file from database
 app.get('/post/id', (req, res) => {
     res.sendFile(__dirname + '/public/views/post_detail_page.html');
 })
 
+// upload post functionality
 app.post('/upload', (req, res) => {
-    // NOTE: req.fields contains all data except file data
-    // NOTE: req.file contains all file related data
+    // NOTE: req.fields contains all data except file data, req.file contains all file related data
     // store image data in variable
     var image = {
-        title: req.fields.title,
+        title: req.fields.title, // title defined in corresponding form, see /views/upload_page.html for further reference
         path: req.files.upload.path
     };
     // TODO: save image infos in database
