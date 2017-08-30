d22a7b7b616d139ed2f2071e1bf505224c7e0b8f
KevinVujic <kevinvujic@yahoo.de>
2017-08-30 15:00:45 +1000

    created language directory for improved layering

---
+++ src/language/english_language.js
@@ -0,0 +1,18 @@
+/**
+ * This file includes the english language object.
+ * If you want to expose text to the user in english
+ * insert a key value pair here.
+ *
+ * The object containing all key-value pairs will get
+ * importet in src/language/language.js. Reference the
+ * key from this file in order to represent the string
+ * to the user.
+ * */
+
+let englishLanguage = {
+
+};
+
+module.exprots = {
+  english: englishLanguage
+};
