e0776b9dd97ffa9a1a959e3413f43f32369a2d6e
KevinVujic <kevinvujic@yahoo.de>
2017-09-04 18:04:28 +1000

    reworked hot page

--- src/client/public/js/hotPagePostList.entry.js
+++ src/client/public/js/hotPagePostList.entry.js
@@ -33855,10 +33855,6 @@ function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defi
 var client = new _reactApollo.ApolloClient();
 
 var currentLanguage = "english"; // default, TODO: change when user changes his local language later on
-console.log("LANG: ");
-console.log(_language2.default);
-console.log("ENGLISH TAGS:");
-console.log(_language2.default["english"]);
 
 //Query for retrieving a list of image posts
 var postsListQuery = (0, _reactApollo.gql)(_templateObject);
@@ -33970,7 +33966,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 let languages = {
-  //
+  // reference your imported language here
   english: __WEBPACK_IMPORTED_MODULE_0__english_language__["a" /* default */]
 };
 
@@ -33984,16 +33980,13 @@ let languages = {
 "use strict";
 /**
  * This file includes the english language object.
- * If you want to expose text to the user in english
- * insert a key value pair here.
  *
- * The object containing all key-value pairs will get
- * importet in src/language/language.js. Reference the
- * key from this file in order to represent the string
- * to the user.
+ * All visible text to the user should be referenced
+ * from here with a proper key value pair.
  * */
 
 let english = {
+  // make a key value pair here for every text you wish to display
   upvotes: 'upvotes',
   downvotes: 'downvotes'
 };
