5f111c2fd5ad1874e95381f1d47fbb63695b2f55
KevinVujic <kevinvujic@yahoo.de>
2017-09-10 18:22:25 +1000

    Implemented form in login page

--- client/src/components/LoginPage/login_page.jsx
+++ client/src/components/LoginPage/login_page.jsx
@@ -4,22 +4,45 @@ import {render} from 'react-dom';
 class LoginPage extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    // call this event when a form field changes its value
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value
+    });
   }
 
   handleSubmit(event) {
     // call this when form gets send to destination
+    console.log("Submit has been called in Login Page!");
   }
 
   render () {
     return (
-      <div>
-        <!-- TODO: implement original rendering functionality -->
-        <p> This text is rendered by the login Page</p>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Username:
+          <input name="username" type="text" value={this.state.username} onChange={this.handleChange} />
+        </label>
+
+        <label>
+          Password:
+          <input name="password" type="password" value={this.state.password} onChange={this.handleChange} />
+        </label>
+
+        <input type="submit" value="submit" />
+      </form>
     );
   }
 }
