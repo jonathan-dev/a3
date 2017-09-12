8297fe22f52e4c47c4bd27f5a7715e41a75a2a0d
Harrison Smith <harrison.pj.smith@gmail.com>
2017-09-12 10:45:04 +1000

    Fixed register page

---
+++ client/src/components/RegisterPage/register_page.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class RegisterPage extends React.Component {
+
+  //Prepare class
+  constructor (props) {
+    super(props);
+
+    //Set up state variable with all necessary fields
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      password2: ''
+    };
+
+    //Bind events to class methods
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  //Handles changes in the input fields on the page
+  handleChange (event) {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value
+    });
+
+    //TODO: Check username not already in use
+    //TODO: Check passwords match, display error message if not
+    //TODO: Check password complexity
+  }
+
+  //Handles submission of register form
+  handleSubmit (event) {
+    event.preventDefault(); //Stops page refresh
+
+    //TODO: remove log statements
+    console.log("Submitted registration request", )
+  }
+
+  //Render HTML register form
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Username:
+          <input name="username" type="text" value={this.state.username} onChange={this.handleChange} />
+        </label>
+
+        <label>
+          Email address:
+          <input name="email" type="text" value={this.state.email} onChange={this.handleChange} />
+        </label>
+
+        <label>
+          Password:
+          <input name="password" type="password" value={this.state.password} onChange={this.handleChange} />
+        </label>
+
+        <label>
+          Re-enter your password:
+          <input name="password2" type="password" value={this.state.password2} onChange={this.handleChange} />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default RegisterPage;
