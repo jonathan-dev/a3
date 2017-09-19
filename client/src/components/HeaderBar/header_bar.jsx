d3370635bfef4a6d229a9680c9b66b60c0552e64
KevinVujic <kevinvujic@yahoo.de>
2017-09-19 20:51:39 +1000

    added commenting in header_bar.jsx

--- client/src/components/HeaderBar/header_bar.jsx
+++ client/src/components/HeaderBar/header_bar.jsx
@@ -1,9 +1,13 @@
+/**
+ * This file contains all links for the header bar of the react application
+ * */
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
 class HeaderBar extends React.Component {
   render () {
+    // enter all your links in the render method below
     return (
       <header>
         <nav>
