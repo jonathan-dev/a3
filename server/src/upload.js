646b018bcc16749db3fc23ad7b7e444dfe566ae4
Jonathan Drude <jonathan.drude@mni.thm.de>
2017-09-27 20:13:49 +1000

    formatted

--- server/src/upload.js
+++ server/src/upload.js
@@ -3,38 +3,38 @@ import md5 from 'md5'
 
 module.exports = function (app) {
 
-  // image upload
-  app.post('/upload', function (req, res) {
-    let form = new formidable.IncomingForm();
-    let imgId;
+    // image upload
+    app.post('/upload', function (req, res) {
+        let form = new formidable.IncomingForm();
+        let imgId;
 
-    form.parse(req);
+        form.parse(req);
 
-    form.on('field', function (name, value) {
-      console.log('field: ', name, value)
+        form.on('field', function (name, value) {
+            console.log('field: ', name, value)
+        });
+
+        form.on('fileBegin', function (name, file) {
+            console.log('fileBegin')
+            // generate img id
+            imgId = md5(file.name + new Date().toString());
+            file.path = __dirname + '/../public/uploads/images/' + imgId + '.png';
+        });
+
+        form.on('file', function (name, file) {
+            console.log('file')
+        });
+
+        form.on('end', function () {
+            console.log('end')
+            res.json({
+                imageId: imgId
+            });
+        });
+
+        form.on('error', function () {
+            console.log('error')
+            res.end('Something went wrong on there server side. Your file may not have yet uploaded.');
+        });
     });
-
-    form.on('fileBegin', function (name, file) {
-      console.log('fileBegin')
-      // generate img id
-      imgId = md5(file.name + new Date().toString());
-      file.path = __dirname + '/../public/uploads/images/' + imgId + '.png';
-    });
-
-    form.on('file', function (name, file) {
-      console.log('file')
-    });
-
-    form.on('end', function () {
-      console.log('end')
-      res.json({
-        imageId: imgId
-      });
-    });
-
-    form.on('error', function () {
-      console.log('error')
-      res.end('Something went wrong on there server side. Your file may not have yet uploaded.');
-    });
-  });
 }
