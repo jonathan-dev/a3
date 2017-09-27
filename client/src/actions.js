198039427e3d1cb1be1c4c8ef81db4841eb32c94
KevinVujic <kevinvujic@yahoo.de>
2017-09-27 20:00:21 +1000

    created actions.js for reducers usage

---
+++ client/src/actions.js
@@ -0,0 +1,5 @@
+/**
+ * File for declaring actions in our application.
+ * Actions are payloads of information that send data from our application to our store.
+ * See the react redux actions documentation for further details
+ * */
