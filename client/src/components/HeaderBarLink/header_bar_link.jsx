1f213f188a0fc515c9a8047ec01af3a134adac52
KevinVujic <kevinvujic@yahoo.de>
2017-09-29 21:35:22 +1000

    reworked header bar link component

--- client/src/components/HeaderBarLink/header_bar_link.jsx
+++ client/src/components/HeaderBarLink/header_bar_link.jsx
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-
 class HeaderBarLink extends Component {
-  constructor (props) {
-    super (props);
-  }
-
   render () {
     return (
       <Link onClick={this.props.onClick} to={this.props.path}>{this.props.text}</Link>
