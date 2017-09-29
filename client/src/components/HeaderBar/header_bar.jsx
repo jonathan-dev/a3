a045a83efa60e57dbea1e8514b452f02f507a8ba
KevinVujic <kevinvujic@yahoo.de>
2017-09-29 21:34:38 +1000

    Reworked header bar component, installed PropTypes typechecking

--- client/src/components/HeaderBar/header_bar.jsx
+++ client/src/components/HeaderBar/header_bar.jsx
@@ -2,26 +2,29 @@
  * This file contains all links for the header bar of the react application
  * */
 
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class HeaderBar extends Component {
+    render() {
+        let headerBarLinks = this.props.headerBarLinks.map((link, index) =>
+            <div key={index}>
+                <Link to={link.path}>{link.text}</Link>
+                <br/>
+            </div>
+        );
 
-  constructor(props) {
-    super(props);
-  }
-
-  render () {
-    if (this.props.headerBarLinks) {
-      let headerBarLinks = this.props.headerBarLinks.map((link, index) => <Link to={link.path} key={index}>{link.text}  </Link>);
-      return (
-        <div>
-          {headerBarLinks}
-        </div>
-      );
+        return (
+            <div>
+                {headerBarLinks}
+            </div>
+        );
     }
-    return <h1>nothing to see in this header bar</h1>
-  }
 }
+
+HeaderBar.proptypes = {
+    headerBarLinks: PropTypes.element.isRequired
+};
 
 export default HeaderBar;
