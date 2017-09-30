d5861e05a84410960c6053a14c889e4dc8444ecf
Jonathan Drude <jonathan.drude@mni.thm.de>
2017-09-30 15:16:54 +1000

    added bootstrap and forgot validation

--- client/src/components/forgot_page.jsx
+++ client/src/components/forgot_page.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import { Form, FormGroup, ControlLabel, InputGroup, FormControl, Button } from 'react-bootstrap';
+
 class ForgotPage extends Component {
 
     constructor(props) {
@@ -8,14 +10,41 @@ class ForgotPage extends Component {
 
         this.state = {
             email: '',
+            emailValid: null,
+            formValid: false
         };
     }
 
+    validateField(fieldName, value) {
+        let fieldValidationErrors = this.state.formErrors;
+        let emailValid = this.state.emailValid;
+
+        switch(fieldName) {
+          case 'email':
+            emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+            emailValid = emailValid ? 'success' : 'error';
+            break;
+          default:
+            break;
+        }
+        this.setState({formErrors: fieldValidationErrors,
+                        emailValid: emailValid,
+                      }, this.validateForm);
+      }
+
+    validateForm() {
+        this.setState({formValid: this.state.emailValid === 'success'});
+    }
+
+
     handleChange = (event) => {
         const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        });
+        this.setState(
+            {
+                [name]: value},
+                () => { this.validateField(name, value)
+            }
+        );
     }
 
     handleSubmit = (event) => {
@@ -30,13 +59,20 @@ class ForgotPage extends Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Email:
-                    <input name="email" type="email" value={this.state.email} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="submit" />
-            </form>
+            <Form inline onSubmit={this.handleSubmit}>
+                <FormGroup controlId="formInlineName" validationState={this.state.emailValid}>
+                    <ControlLabel>Email:</ControlLabel>
+                    {' '}
+                    <InputGroup>
+                        <InputGroup.Addon>@</InputGroup.Addon>
+                        <FormControl name="email" type="email" value={this.state.email} onChange={this.handleChange} />
+                    </InputGroup>
+                    <FormControl.Feedback />
+                </FormGroup>
+                    {' '}
+                <Button type="submit" bsStyle="primary" disabled={this.state.formValid?false:true}>Send</Button>
+            </Form>
+
         );
     }
 }
