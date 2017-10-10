ba6d02b64147eceab493ebf2c6dc5f43d573f8c3
Harrison Smith <harrison.pj.smith@gmail.com>
2017-10-10 22:58:23 +1100

    Added ban function to admin page

--- client/src/components/admin_page.jsx
+++ client/src/components/admin_page.jsx
@@ -34,24 +34,74 @@ const AdminPage = props => {
     if (data) {
         const { loading, error, users } = data;
 
+        //Cell editing mode for react bootstrap table
+        // const cellEdit = {
+        //     mode: 'click' // click cell to edit
+        // };
+
+        function banUserMutation(userid) {
+            //Send a mutation request to server
+            props.mutate({
+                variables: {
+                    userid: userid
+                },
+                refetchQueries: [{ query: usersListQuery }]
+            }).then(({ data }) => {
+                console.log('got data, user banned', data);
+            }).catch((error) => {
+                console.log('there was an error sending the query', error);
+            });
+        }
+
+        function unbanUserButton(cell, row, rowIndex) {
+            //Submit unban
+            console.log('Banning user id ', row.id);
+        }
+
+        function banButton(cell, row, enumObject, rowIndex) {
+            return  (!row.isLocked ) ? (
+                <Button
+                    block
+                    onClick={() =>
+                    banUserMutation(row.id)}>
+                    Ban { row.username }
+                </Button>
+            ) : (
+                <Button
+                    block
+                    onClick={() =>
+                    unbanUser(cell, row, rowIndex)}>
+                    Unban { row.username }
+                </Button>
+            )
+        }
+
         if (loading) {
             return <div>Loading</div>
         }
+
         if (error) {
             return <div>Error</div>
         }
 
         return (
             <section className="col-lg-8" style={colCentered}>
-                <BootstrapTable data={ users } striped={true} search={true} hover={true}>
+                <BootstrapTable data={ users }
+                    striped={true}
+                    search={true}
+                    hover={true}>
                     <TableHeaderColumn dataField="id" isKey={true} dataAlign="left" dataSort={true}>User ID</TableHeaderColumn>
                     <TableHeaderColumn dataField="username" dataSort={true}>Username</TableHeaderColumn>
                     <TableHeaderColumn dataField="isAdmin" dataSort={true}>Administrator</TableHeaderColumn>
                     {/* <TableHeaderColumn
                         dataField="isAdmin"
                         dataSort={true}
-                        editable={ { type: 'checkbox', options: { values: 'Y:N' } } }> Is administrator </TableHeaderColumn> */}
-                    <TableHeaderColumn dataField="bannedUntil" dataSort={true}>Banned until</TableHeaderColumn>
+                        editable={ { type: 'checkbox', options: { values: 'Y:N' } } }>
+                        Is administrator
+                    </TableHeaderColumn> */}
+                    <TableHeaderColumn dataField="isLocked" dataSort={true}>User account locked</TableHeaderColumn>
+                    <TableHeaderColumn dataField="lockUntil" dataSort={true}>Locked until</TableHeaderColumn>
+                    <TableHeaderColumn dataField="button" dataFormat={ banButton }>Ban user</TableHeaderColumn>
                 </BootstrapTable>
             </section>
         )
@@ -59,20 +109,30 @@ const AdminPage = props => {
 
 }
 
-const userToken = localStorage.getItem("token");
-
 //Query for retrieving a list of users
-//need to somehow get the token into here as an argument
 const usersListQuery = gql`
 query userListQuery {
   users {
     id
     username
     isAdmin
+    lockUntil
+    isLocked
   }
 }
 `;
 
-export default graphql(usersListQuery, {
+const UserMutations = gql`
+    mutation banSpecificUser($userid: String!) {
+        banUser(id: $userid) {
+            username
+            lockUntil
+        }
+    }
+`;
+
+export default graphql(UserMutations)(graphql(usersListQuery, {
     options: { pollInterval: 2000 },
-})(AdminPage);
+})(AdminPage));
+
+//export default graphql(UserMutations)(AdminPage);
