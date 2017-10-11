54476b0d925f95d22bd4689b6f37c548494550c0
KevinVujic <kevinvujic@yahoo.de>
2017-10-12 10:16:53 +1100

    implemented raw container functionality for editable comment

---
+++ client/src/containers/editable_comment_container.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import EditableComment from '../components/editable_comment';
+
+
+
+
+const mapStateToProps = (state, ownProps) => {
+    return {};
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {};
+};
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditableComment);
