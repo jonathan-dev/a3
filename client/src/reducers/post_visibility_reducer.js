bac7b1b9812e74d22ee4e18007cba49adaa94876
KevinVujic <kevinvujic@yahoo.de>
2017-10-14 13:31:42 +1100

    added reducer for comment visibility

---
+++ client/src/reducers/post_visibility_reducer.js
@@ -0,0 +1,23 @@
+import {
+    SHOW_POST_COMMENTS,
+    HIDE_POST_COMMENTS
+} from '../constants/action_types';
+
+const initialVisibilityState = {
+    visiblePostComments: []
+};
+
+export function postVisibility (state = initialVisibilityState, action) {
+    switch (action.type) {
+        case SHOW_POST_COMMENTS:
+            return Object.assign({}, state, {
+                visiblePostComments: state.visiblePostComments.concat(action.post)
+            });
+        case HIDE_POST_COMMENTS:
+            return Object.assign({}, state, {
+                visiblePostComments: state.visiblePostComments.filter(post => post.id != action.post.id)
+            });
+        default:
+            return state;
+    }
+}
