d82b491ee73be18f56ffd311c38a377bd33fda1a
KevinVujic <kevinvujic@yahoo.de>
2017-10-14 18:13:56 +1100

    removed unused functions in postcontainer

--- client/src/containers/post_container.js
+++ client/src/containers/post_container.js
@@ -2,14 +2,6 @@ import { connect } from 'react-redux';
 import Post from '../components/post';
 import { showPostComments, hidePostComments } from '../actions/actions';
 
-const a = (post, dispatch) => {
-    dispatch(showPostComments(post));
-};
-
-const b = (post, dispatch) => {
-    dispatch(hidePostComments(post));
-};
-
 const mapStateToProps = (state, ownProps) => {
     return {
         post: ownProps.post,
@@ -19,8 +11,8 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        showPostComments: post => a(post, dispatch),
-        hidePostComments: post => b(post, dispatch)
+        showPostComments: post => dispatch(showPostComments(post)),
+        hidePostComments: post => dispatch(hidePostComments(post))
     };
 };
 
