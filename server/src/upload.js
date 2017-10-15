e4aae319e65e621c9c02c566d31a86db3bd99942
KevinVujic <kevinvujic@yahoo.de>
2017-10-15 13:17:15 +1100

    merged back to master

--- server/src/upload.js
+++ server/src/upload.js
@@ -10,30 +10,19 @@ module.exports = function (app) {
 
         form.parse(req);
 
-        form.on('field', function (name, value) {
-            console.log('field: ', name, value)
-        });
-
         form.on('fileBegin', function (name, file) {
-            console.log('fileBegin')
             // generate img id
             imgId = md5(file.name + new Date().toString());
             file.path = __dirname + '/../public/uploads/images/' + imgId + '.png';
         });
 
-        form.on('file', function (name, file) {
-            console.log('file')
-        });
-
         form.on('end', function () {
-            console.log('end')
             res.json({
                 imageId: imgId
             });
         });
 
         form.on('error', function () {
-            console.log('error')
             res.end('Something went wrong on there server side. Your file may not have yet uploaded.');
         });
     });
