085a1671c4f074f405869a453d548f60c8aa1c68
Jonathan Drude <jonathan.drude@mni.thm.de>
2017-10-15 16:32:08 +1100

    added https and deployment specific scripts

--- server/config/key.js
+++ server/config/key.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
-import path from'path';
+let fs = require('fs');
+let path = require('path');
 const key = fs.readFileSync(path.join(__dirname,'./priv_key.pem'), 'utf-8')
-export default key
+module.exports = key;
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import fs from 'fs';
+// import fs from 'path';
+import https from 'https';
+import httpsRedirect from 'express-https-redirect';
+
 import path from 'path';
 import cors from 'cors';
 import graphQLHTTP from 'express-graphql';
@@ -18,6 +23,9 @@ function resolve(dir) {
 //------------------------------------------
 const PORT = 8000;
 const IMAGES_URL = '../public/uploads/images/';
+const privateKey = fs.readFileSync('/home/jonathan/Documents/graphql/server/ssl/test.key')
+const certificate = fs.readFileSync('/home/jonathan/Documents/graphql/server/ssl/test.crt')
+const credentials = {key: privateKey, cert: certificate}
 //------------------------------------------
 
 const app = express();
@@ -26,6 +34,7 @@ const app = express();
 // Configuration
 //------------------------------------------
 
+app.use('/', httpsRedirect(true));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({
     extended: true
@@ -59,4 +68,11 @@ app.get('/*', function (req, res) {
 
 app.listen(PORT, () => {
     console.log(`server listening on http://localhost:${PORT}`);
+});
+
+let httpsServer = https.createServer(credentials, app)
+
+// start server
+httpsServer.listen(443, () => {
+    console.log(`server listening on https://localhost`);
 });
