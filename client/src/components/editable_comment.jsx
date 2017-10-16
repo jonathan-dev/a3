b227a6a145377e5dd49ea6c78a48d220e7887997
KevinVujic <kevinvujic@yahoo.de>
2017-10-16 14:31:54 +1100

    added commenting to editable comment component

--- client/src/components/editable_comment.jsx
+++ client/src/components/editable_comment.jsx
@@ -1,4 +1,12 @@
-import React, {Component} from 'react';
+/**
+ * This file is the visual representation of an editable comment. An editable comment is a comment which the user chose
+ * to edit, thus it needs to be his own comment and he needs to be logged in. Only one comment can be edited at a time.
+ *
+ * All state information needed for rendering, as well as functionality in form of callbacks will be based by the
+ * editable comment container.
+ * */
+
+import React, { Component } from 'react';
 import {
     Form,
     Button,
@@ -9,13 +17,11 @@ import {
 
 class EditableComment extends Component {
 
-    onComponentWillUnmount() {
-
-    }
-
     render () {
+        // extract all variables passed by the corresponding container
         const {editCommentText, onSubmit, onAbort, originalComment, onEditInputChange} = this.props;
 
+        // return the visual representation including an input field for editing the comment
         return (
             <Form horizontal onSubmit={(event) => onSubmit(event, originalComment)}>
                 <h2 className="author">{originalComment.owner.username}</h2>
