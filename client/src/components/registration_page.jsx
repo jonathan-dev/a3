5f3fd0b1300a5a6c49925ef8c63dc2ffa4b1974f
KevinVujic <kevinvujic@yahoo.de>
2017-10-16 15:57:26 +1100

    added commenting to the registration component

--- client/src/components/registration_page.jsx
+++ client/src/components/registration_page.jsx
@@ -1,3 +1,9 @@
+/**
+ * This component contains the visual representation of the Registration page. All state information needed as well
+ * as the functionality behind functions will be passed to this component as either variables or callbacks to be invoked
+ * whenever needed by it's wrapping component and container class.
+ * */
+
 import React, { Component } from 'react';
 import inputField from './input_field'
 import {
@@ -13,25 +19,30 @@ import { LOGIN_PATH } from "../constants/paths";
 
 class RegistrationPage extends Component {
 
+    // once the user leaves this page clear the authentification state as he needs to login after registration
     componentWillUnmount () {
         this.props.clearAuthenticationState();
     }
 
     render () {
-        const { pristine, submitting, invalid, registrationCompleted } = this.props;
+        // extract the data handed to this component
+        const { pristine, submitting, invalid, registrationCompleted, handleSubmit } = this.props;
 
+        // once registration has been completed, reroute the user to the login page
         if (registrationCompleted) {
             return <Redirect to={LOGIN_PATH}/>;
         }
 
+        // style object used for rendering as seen below
         const colCentered = {
             float: 'none',
             margin: '0 auto',
         };
 
+        // return the visual representation adjusted with the given props variables / callbacks
         return (
             <Panel className="col-lg-4" style={colCentered}>
-                <Form horizontal onSubmit={this.props.handleSubmit}>
+                <Form horizontal onSubmit={handleSubmit}>
                     <Field
                         name="username"
                         component={inputField}
