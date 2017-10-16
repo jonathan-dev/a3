21c79fa800fc7ed978ae21140c38273cd23ea2c7
KevinVujic <kevinvujic@yahoo.de>
2017-10-16 14:35:16 +1100

    added commenting to forgot password page

--- client/src/components/forgot_password_page.jsx
+++ client/src/components/forgot_password_page.jsx
@@ -1,7 +1,12 @@
-import React, { Component }from 'react'
-import { Field } from 'redux-form'
+/**
+ * This file is the visual representation of the forgot password page. All necessary state information for rendering
+ * as well as functionality in form of callbacks will be passed by its corresponding container.
+ * */
+
+import React, {Component} from 'react'
+import {Field} from 'redux-form'
 import inputField from './input_field'
-import { LinkContainer } from 'react-router-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 import {
     Form,
     FormGroup,
@@ -12,43 +17,48 @@ import {
 
 class ForgotPage extends Component {
 
-    componentWillUnmount() {
+    // Clear the state information set by this page once the user leaves it
+    componentWillUnmount () {
         this.props.resetResetInfo()
     }
 
-    render(){
-        const { handleSubmit, pristine, reset, submitting, invalid, resetInfo } = this.props
+    render () {
+        // extract all variables passed by the forgot password container
+        const {handleSubmit, pristine, submitting, invalid, resetInfo} = this.props;
 
-    const colCentered = {
-        float: 'none',
-        margin: '0 auto',
-    }
+        // style object used for rendering as seen below
+        const colCentered = {
+            float: 'none',
+            margin: '0 auto',
+        };
 
-    return (
-        <Panel className="col-lg-4" style={colCentered}>
-            <Form horizontal onSubmit={handleSubmit}>
-                <Field
-                    name="email"
-                    component={inputField}
-                    label="Email"
-                    type="text"
-                />
-                <FormGroup>
-                    <Col smOffset={2} sm={10}>
-                        <Button type="submit" bsStyle="primary" disabled={pristine || submitting || invalid}>Send</Button>
-                    </Col>
-                </FormGroup>
-            </Form>
-            {resetInfo && resetInfo === 'success' &&
+        // return the visual representation computed with the passed information
+        return (
+            <Panel className="col-lg-4" style={colCentered}>
+                <Form horizontal onSubmit={handleSubmit}>
+                    <Field
+                        name="email"
+                        component={inputField}
+                        label="Email"
+                        type="text"
+                    />
+                    <FormGroup>
+                        <Col smOffset={2} sm={10}>
+                            <Button type="submit" bsStyle="primary"
+                                    disabled={pristine || submitting || invalid}>Send</Button>
+                        </Col>
+                    </FormGroup>
+                </Form>
+                {resetInfo && resetInfo === 'success' &&
                 <div>
-                    <LinkContainer to='/' >
+                    <LinkContainer to='/'>
                         <a href={'/'}>Retun to Home</a>
                     </LinkContainer>
                     <p>request was send successfully! check your emails</p>
                 </div>
-            }
-        </Panel>
-    );
+                }
+            </Panel>
+        );
     }
 }
 
