f01a5b522007ed0ddafb9ab1ab9c99c34d72cebe
KevinVujic <kevinvujic@yahoo.de>
2017-10-16 16:25:42 +1100

    reworked email creation to create correct link

--- server/src/authentication.js
+++ server/src/authentication.js
@@ -160,7 +160,7 @@ export function authApp(app) {
                 .then(() => {
                     const text = 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
                         'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                        'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+                        'http://www.studio17.com.au/reset/' + token + '\n\n' +
                         'If you did not request this, please ignore this email and your password will remain unchanged.\n';
                     sendMail(email, text);
                     res.send('send')
