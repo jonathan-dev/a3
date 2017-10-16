3dbf1419f0c5e8e1b9d9742a422c0e55fbc51940
KevinVujic <kevinvujic@yahoo.de>
2017-10-16 14:32:27 +1100

    renamed forgot page to forgot password page

--- client/src/containers/forgot_page_container.js
+++ client/src/containers/forgot_page_container.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {
     connect
 } from 'react-redux';
-import ForgotPage from '../components/forgot_page'
+import ForgotPage from '../components/forgot_password_page'
 import {
     requestPasswordReset,
     resetRequestPasswordRequest
