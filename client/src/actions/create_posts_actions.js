42a9855cbcdd46707bdcc62c1b66532f60582f28
Harrison Smith <harrison.pj.smith@gmail.com>
2017-10-16 16:48:42 +1100

    Commented create_posts_actions

--- client/src/actions/create_posts_actions.js
+++ client/src/actions/create_posts_actions.js
@@ -1,7 +1,8 @@
 /**
- * This file contains all the action generators for creating posts, usage can be seen in the create post container
- * */
-
+ * Declares action generators specifically for making a post.
+ * Usage can be seen in the create post container
+ * See actions.js for more info.
+ */
 import {
     UPLOAD_IMAGE,
     UPDATE_TAGS,
@@ -10,6 +11,10 @@ import {
     RESET_CREATE_POST_STATE
 } from '../constants/action_types'
 
+/**
+ * Generates action for updating the image upload progres
+ * @param {Number} progress - Progress amount (between 1 and 100)
+ */
 export function updateUploadProgress(progress) {
     return {
         type: UPDATE_UPLOAD_PROGRESS,
@@ -17,6 +22,11 @@ export function updateUploadProgress(progress) {
     }
 }
 
+/**
+ * Uploads a given image
+ * @param {ReduxDispatch} dispatch - Redux dispatch object to dispatch action to
+ * @param {FormData} FormData - Object, has image and form data
+ */
 export function uploadImage(dispatch, FormData) {
     return {
         type: UPLOAD_IMAGE,
@@ -37,6 +47,10 @@ export function uploadImage(dispatch, FormData) {
     }
 }
 
+/**
+ * Action generator for modifying the new post's tags
+ * @param {Tags} tags - Json obj, contains tag name
+ */
 export function updateTags(tags) {
     return {
         type: UPDATE_TAGS,
@@ -44,6 +58,10 @@ export function updateTags(tags) {
     }
 }
 
+/**
+ * Action generator for updating an image
+ * @param {Image} image - Image data stored in json obj
+ */
 export function updateImage(image) {
     return {
         type: UPDATE_IMAGE,
@@ -51,6 +69,9 @@ export function updateImage(image) {
     }
 }
 
+/**
+ * Resets the create post state
+ */
 export function resetState(){
     return {
         type: RESET_CREATE_POST_STATE
